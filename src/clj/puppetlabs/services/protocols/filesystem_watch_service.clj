7b573207dac038dba2f531122c9f771b275ece1e
Kevin Corcoran <kevin.corcoran@puppetlabs.com>
2016-07-15 15:35:04 +0100

    (TK-384) Cleanup docstrings on protocol

--- src/clj/puppetlabs/services/protocols/filesystem_watch_service.clj
+++ src/clj/puppetlabs/services/protocols/filesystem_watch_service.clj
@@ -10,28 +10,31 @@
 ;; TODO: add schema for Watcher {:watch-service WatchService :callbacks (schema/atom [IFn])}
 
 (defprotocol FilesystemWatchService
-  (create-watcher!
-    [this]
-    "TODO")
-  (add-callback! [this watcher callback]
-   "TODO")
+
+  (create-watcher! [this]
+    "Returns a Watcher which can be used to initiate watching of a directory on
+    the filesystem.  The Watcher is an opaque object of unspecified type.  A
+    Watcher must be passed in as an argument to subsequent calls on this
+    service but nothing else can be done with it.")
+
   (add-watch-dir! [this watcher dir options]
-   "TODO")
+    "Given a Watcher and a directory on the filesystem, initiate watching of
+    dir.  The watcher's callbacks will be invoked when dir changes.  Available
+    options are:
+      * :recursive true   - If true, callbacks will be invoked when dir or any
+                            file underneath dir changes.")
 
+  (add-callback! [this watcher callback]
+    "Adds a callback to a Watcher.  The callback will be invoked when any
+    watched directories change.  The callback will be passed a sequence of
+    Events as its only argument.  The exact events are unspecified and possibly
+    platform-dependent; however, the following events are guaranteed to be
+    passed to the callback
 
+     * an event of :type :create with :path p, when a file is created at path p
+     * an event of :type :modify with :path p, when the contents of a file at path p are modified
+     * an event of :type :delete with :path p, when a file is deleted at path p
 
-  ;"Begins (recursive) monitoring of a directory on the filesystem.  callback
-  ;will be invoked when path, or any files underneath it, change.  The callback
-  ;will be passed a sequence of Events as its only argument.  The exact events
-  ;are unspecified and possibly platform-dependent; however, the following
-  ;events are guaranteed to be passed to the callback
-  ;
-  ; * an event of :type :create with :path p, when a file is created at path p
-  ; * an event of :type :modify with :path p, when the contents of a file at path p are modified
-  ; * an event of :type :delete with :path p, when a file is deleted at path p
-  ;
-  ;Note that, for any of those particular changes, there may also be additional
-  ;events passed to the callback, such as events on a parent directory of a
-  ;changed file."
-
-  )
+    Note that, for any of those particular changes, there may also be additional
+    events passed to the callback, such as events on a parent directory of a
+    changed file."))
