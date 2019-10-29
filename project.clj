8dc1ee3182166d58ea5dd2790446bc7509758eb9
Jenkins CI <ci@puppet.com>
2019-10-29 23:05:31 +0000

    Version 1.2.2-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject puppetlabs/trapperkeeper-filesystem-watcher "1.2.1"
+(defproject puppetlabs/trapperkeeper-filesystem-watcher "1.2.2-SNAPSHOT"
   :description "Trapperkeeper filesystem watcher service"
   :url "https://github.com/puppetlabs/trapperkeeper-filesystem-watcher"
   :license {:name "Apache License, Version 2.0"
